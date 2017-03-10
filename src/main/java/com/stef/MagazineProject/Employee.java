3eb8486132b950e85aa926a868e0ba4d0243974f
OliaStfn <kardash44@mail.ru>
2017-03-10 19:05:36 +0200

    Task 8002

--- src/main/java/com/stef/MagazineProject/Employee.java
+++ src/main/java/com/stef/MagazineProject/Employee.java
@@ -1,6 +1,7 @@
 package com.stef.MagazineProject;
 
 import java.util.GregorianCalendar;
+import java.util.Vector;
 
 public class Employee extends Human {
 
@@ -8,13 +9,14 @@ public class Employee extends Human {
     private GregorianCalendar date_of_begin;
     private double salary;
     private double rating;
-
+    private Vector<Mark> marks;
 
     public Employee() {
         super();
         salary = 2000;
         rating = 0;
         date_of_begin = new GregorianCalendar(2016, 5, 5);
+        marks= new Vector<Mark>();
     }
 
     public Employee(String name, String surname, int year, int month, int day, double salary, double rating) {
@@ -22,6 +24,7 @@ public class Employee extends Human {
         this.salary = salary;
         this.rating = rating;
         date_of_begin = new GregorianCalendar();
+        marks= new Vector<Mark>();
     }
 
     public void raiseSalary() {
@@ -43,8 +46,8 @@ public class Employee extends Human {
         minutes = curentDate.get(GregorianCalendar.MINUTE)
                 - date_of_begin.get(GregorianCalendar.MINUTE);
 
-        if(month<0){
-            month=0;
+        if (month < 0) {
+            month = 0;
         }
         System.out.println(" year = " + year +
                 " month = " + month +
@@ -72,5 +75,23 @@ public class Employee extends Human {
 
     public GregorianCalendar getDate_of_begin() {
         return date_of_begin;
+    }
+    public void addMark(double mark){
+        marks.add(new Mark(mark));
+    }
+    public double generationRaiting(){
+        double sum=0;
+        int count=0;
+        GregorianCalendar current=new GregorianCalendar();
+        //for(Mark i : marks){
+
+      //  }
+        for(int i=0;i<marks.size();i++){
+            if(marks.elementAt(i).getTime().get(GregorianCalendar.MONTH)==current.get(GregorianCalendar.MONTH)){
+                count++;
+                sum+=marks.elementAt(i).getMark();
+            }
+        }
+        return sum/count;
     }
 }
--- src/main/java/com/stef/MagazineProject/Mark.java
+++ src/main/java/com/stef/MagazineProject/Mark.java
@@ -3,5 +3,24 @@ package com.stef.MagazineProject;
 import java.util.GregorianCalendar;
 
 public class Mark {
+    private double mark;
+    private GregorianCalendar time;
 
+    public Mark() {
+        mark=0;
+        time= new GregorianCalendar();
+    }
+
+    public Mark(double mark) {
+        this.mark = mark;
+        time=new GregorianCalendar();
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public GregorianCalendar getTime() {
+        return time;
+    }
 }
