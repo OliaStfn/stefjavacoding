e51291f323c0e0b3316eb54812c37a257be663a9
OliaStfn <kardash44@mail.ru>
2017-03-11 16:15:08 +0200

    Task 9003 finish

--- src/main/java/com/stef/MagazineProject/Menu.java
+++ src/main/java/com/stef/MagazineProject/Menu.java
@@ -1,6 +1,28 @@
 package com.stef.MagazineProject;
 
 public class Menu {
+    public static void menu{
 
+    int x=0;
+    System.out.println("Welcome to the internet shop");
+    do{
+        System.out.println("You can choose one of the point to continue");
+        System.out.println("1- Add product\n 2- Find product\n 3- Display information about product\n 4- Buy product\n " +
+                "5-Sort products\n 6-Add product to the basket\n 7- Add to list favorites\n" +
+                "0- Exit");
+    }while(x!=0);
+    switch(x){
+        case 1: break;
+        case 2: break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        case 6: break;
+        case 7: break;
+        case 0: break;
+        default:
+
+        }
+}
 }
 
