284e782eead3386c80315710b43e98b65d229714
NAzarMh <nazar.myhailiv@gmail.com>
2017-03-15 12:30:50 +0200

    Code refactoring

--- src/main/java/com/stef/MagazineProject/Manager.java
+++ src/main/java/com/stef/MagazineProject/Manager.java
@@ -1,15 +1,7 @@
 package com.stef.MagazineProject;
 
 public class Manager extends Employee {
-    private int productofStock;
 
-    public int getProductofStock() {
-        return productofStock;
-    }
-
-    public void setProductofStock(int productofStock) {
-        this.productofStock = productofStock;
-    }
 
     @Override
     public void raiseSalary() {
