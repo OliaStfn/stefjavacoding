377b2963c28a24e367fb72e2dafb916d0db328fb
OliaStfn <kardash44@mail.ru>
2017-03-17 14:55:17 +0200

    Task 1001 finish

--- src/main/java/com/stef/MagazineProject/Manager.java
+++ src/main/java/com/stef/MagazineProject/Manager.java
@@ -2,9 +2,39 @@ package com.stef.MagazineProject;
 
 public class Manager extends Employee {
 
-    
+    private int phone;
+    private String department;
+
+    public Manager(int phone, String department) {
+        super();
+        this.phone = phone;
+        this.department = department;
+    }
+
+    public Manager(String name, String surname, int year, int month, int day, double salary, int phone, String department) {
+        super(name, surname, year, month, day, salary);
+        this.phone = phone;
+        this.department = department;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
     @Override
     public void raiseSalary() {
-        setSalary(getSalary()+getSalary()*0.15);
+        setSalary(getSalary() + getSalary() * 0.15);
     }
 }
