49e5f46a891238e26be118534458f59c568b2850
OliaStfn <kardash44@mail.ru>
2017-03-17 14:51:18 +0200

    Task 1001 start

--- src/main/java/com/stef/MagazineProject/Manager.java
+++ src/main/java/com/stef/MagazineProject/Manager.java
@@ -2,7 +2,7 @@ package com.stef.MagazineProject;
 
 public class Manager extends Employee {
 
-
+    
     @Override
     public void raiseSalary() {
         setSalary(getSalary()+getSalary()*0.15);
