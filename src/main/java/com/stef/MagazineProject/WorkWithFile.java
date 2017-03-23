02df40fc47144b092a40170274c2f8b5f956717f
OliaStfn <kardash44@mail.ru>
2017-03-23 17:23:58 +0200

    Task 12001 finish

--- src/main/java/com/stef/MagazineProject/WorkWithFile.java
+++ src/main/java/com/stef/MagazineProject/WorkWithFile.java
@@ -1,6 +1,52 @@
 package com.stef.MagazineProject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class WorkWithFile {
 
+    public FileInputStream testMethod1() {
 
+        File file = new File("test.txt");
+
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+
+            fileInputStream.read();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+
+                if (fileInputStream != null) {
+
+                    fileInputStream.close();
+
+                }
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+        return fileInputStream;
+
+    }
+
+    public static void main(String[] args) {
+
+        WorkWithFile instance1 = new WorkWithFile();
+
+        instance1.testMethod1();
+
+    }
 }
