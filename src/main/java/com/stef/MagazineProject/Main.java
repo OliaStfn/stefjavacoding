36dd716b589b19495c28674fe90027988e604f7d
OliaStfn <kardash44@mail.ru>
2017-03-26 19:18:00 +0300

    Task 1301 start

--- src/main/java/com/stef/MagazineProject/Main.java
+++ src/main/java/com/stef/MagazineProject/Main.java
@@ -4,13 +4,6 @@ import java.util.GregorianCalendar;
 
 public class Main {
     public static void main(String[] args) {
-       // Human obj = new Human();
-       // Human object = new Human("John", "Brown",1994,11,3);
-       // obj.setName("Olia");
-       // obj.setSurname("Stefanyshyn");
-
-       // String name = object.getName();
-       // String surname = object.getSurname();
 
         Employee em = new Manager();
         GregorianCalendar born=null;
