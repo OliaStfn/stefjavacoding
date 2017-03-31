05db9791da1a22d760c11de077df9d17c185fc20
OliaStfn <kardash44@mail.ru>
2017-03-31 17:14:15 +0300

    Task 1401 finish

--- src/main/java/com/stef/Meetings/Task1302/Stock.java
+++ src/main/java/com/stef/Meetings/Task1302/Stock.java
@@ -1,24 +1,38 @@
 package com.stef.Meetings.Task1302;
 
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Stock {
+    private static final Logger log = Logger.getLogger(Stock.class);
     private static ArrayList<Product> goods = new ArrayList<Product>();
 
     public static void addProduct(String desc, double price) {
+        try {
+            if (price <= 0) {
+                throw new Exception();
+            }
             goods.add(new Product(desc, price));
+        } catch (Exception e) {
+            System.out.println("Price less than zero");
+            log.info("Price less than zero" + e.getMessage());
+        }
+
+
     }
-    public static Product findProduct(){
-        for(Product product : goods){
+
+    public static Product findProduct() {
+        for (Product product : goods) {
             System.out.println(product.toString());
         }
         System.out.print("Chose your product by ID:");
         Scanner in = new Scanner(System.in);
-        int inputID=in.nextInt();
-        for (int i = 0; i <goods.size() ; i++) {
-            if(goods.get(i).getItemID()==inputID){
-                inputID=i;
+        int inputID = in.nextInt();
+        for (int i = 0; i < goods.size(); i++) {
+            if (goods.get(i).getItemID() == inputID) {
+                inputID = i;
                 break;
             }
         }
