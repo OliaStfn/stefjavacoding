ea8ee022e552dfcdea78b995a4ad5cab55455fc1
OliaStfn <kardash44@mail.ru>
2017-04-23 13:30:47 +0300

    finish 20.01

--- src/main/java/com/stef/Meetings/lesson20_JDBC/DataBaseWork.java
+++ src/main/java/com/stef/Meetings/lesson20_JDBC/DataBaseWork.java
@@ -1,9 +1,12 @@
 package com.stef.Meetings.lesson20_JDBC;
 
+import org.apache.log4j.Logger;
+
 import java.sql.*;
 
 public class DataBaseWork {
-    private static String diverName = "com.mysql.jdbc.Driver";
+    private static final Logger log = Logger.getLogger(DataBaseWork.class);
+    private static String driverName = "com.mysql.jdbc.Driver";
     private static String URL = "jdbc:mysql://servlab.mysql.ukraine.com.ua:3306/servlab_devolga";
     private static String USERNAME = "servlab_devolga";
     private static String PASSWORD = "942lucxf";
@@ -13,16 +16,19 @@ public class DataBaseWork {
         Connection connection = null;
 
         try {
-            Class.forName(diverName); // Завантажуємо клас драйвера
-
+            Class.forName(driverName); // Завантажуємо клас драйвера
+            log.info("We get driver JDBC");
         } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
+            log.error("Driver JDBC has NOT get");
+            log.error(e.getMessage());
         }
 
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("The successful connection for DB");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            log.error("Failed connection for DB");
+            log.error(e.getMessage());
         }
         return connection;
     }
