38bd03ee93daee6f051e4d0381fd949e368c7757
NazarMh <nazar.myhailiv@gmail.com>
2017-04-23 18:08:36 +0300

    Meeting 21 - CRUD JDBC

--- src/main/java/com/stef/Meetings/lesson20_JDBC/DataBaseWork.java
+++ src/main/java/com/stef/Meetings/lesson20_JDBC/DataBaseWork.java
@@ -1,5 +1,6 @@
 package com.stef.Meetings.lesson20_JDBC;
 
+import com.stef.MagazineProject.Product;
 import org.apache.log4j.Logger;
 
 import java.sql.*;
@@ -32,5 +33,85 @@ public class DataBaseWork {
         }
         return connection;
     }
+
+    public static void addNewProduct() throws SQLException {
+        Connection con = null;
+        Statement stm = null;
+
+        String sql_query = "INSERT INTO goods"
+                + "(ID,NAME,PRICE,VENDOR,PRODUCTIONDATE,EXPDATE)"
+                + " VALUES (6, 'burger', 40.50, 'RoyalBurger', '2017-4-23', '2017-4-27')";
+
+
+        try {
+            con = getConnection();
+            stm = con.createStatement();
+            stm.executeUpdate(sql_query);
+            log.info("Product was added successfully");
+        } catch (SQLException e) {
+            log.error("Product wasn't added");
+            log.error(e.getMessage());
+        } finally {
+            if (con != null) con.close();
+            if (stm != null) stm.close();
+        }
+
+
+    }
+
+    public static void updateProduct() throws SQLException {
+        Connection con = null;
+        Statement stm = null;
+
+        String sql_query = "UPDATE goods SET price=27 WHERE name='burger'";
+
+        try {
+            con = getConnection();
+            stm = con.createStatement();
+            stm.executeUpdate(sql_query);
+            log.info("Product was updated successfully");
+        } catch (SQLException e) {
+            log.error("Product wasn't updated");
+            log.error(e.getMessage());
+        } finally {
+            if (con != null) con.close();
+            if (stm != null) stm.close();
+        }
+
+    }
+
+
+    public static void getProducts() throws SQLException {
+        Connection con = null;
+        Statement stm = null;
+
+        String sql_query = "SELECT * FROM goods";
+
+        try {
+            con = getConnection();
+            stm = con.createStatement();
+            ResultSet rs = stm.executeQuery(sql_query);
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                double price = rs.getDouble("price");
+                String vendor = rs.getString("vendor");
+                Date prDate = rs.getDate("productionDate");
+                Date expDate = rs.getDate("expDate");
+
+                System.out.println(id + "\t" + name + "\t" + price + "\t" + vendor + "\t" + prDate + "\t" + expDate);
+            }
+        } catch (SQLException e) {
+            log.error("Error in getting products from DB");
+            log.error(e.getMessage());
+        } finally {
+            if (con != null) con.close();
+            if (stm != null) stm.close();
+        }
+
+
+    }
+
 
 }
--- src/test/java/com/stef/Meetings/lesson20_JDBC/DataBaseWorkTest.java
+++ src/test/java/com/stef/Meetings/lesson20_JDBC/DataBaseWorkTest.java
@@ -5,9 +5,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DataBaseWorkTest {
+    @Test
+    public void getProducts() throws Exception {
+        DataBaseWork.getProducts();
+    }
 
     @Test
-    public void connectionTest() throws Exception{
+    public void updateProduct() throws Exception {
+        DataBaseWork.updateProduct();
+    }
+
+    @Test
+    public void addNewProduct() throws Exception {
+        DataBaseWork.addNewProduct();
+    }
+
+    @Test
+    public void connectionTest() throws Exception {
         assertNotNull(DataBaseWork.getConnection());
     }
 
