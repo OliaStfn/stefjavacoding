9d46f93c8edcb16e49f8d9798e2fed92fe7158f9
OliaStfn <kardash44@mail.ru>
2017-05-05 15:21:11 +0300

    finish 23.01

--- src/main/java/com/stef/Meetings/lesson23_Generics/SelectionSort.java
+++ src/main/java/com/stef/Meetings/lesson23_Generics/SelectionSort.java
@@ -1,5 +1,32 @@
 package com.stef.Meetings.lesson23_Generics;
 
 
-public class SelectionSort {
+public class SelectionSort<T extends Number> {
+    private int indexMin;
+    private int size;
+    private T temp;
+    private T[] arr;
+
+    public SelectionSort(T[] arr) {
+        this.arr = arr;
+        indexMin = 0;
+        size = arr.length;
+    }
+
+    public T[] selectionSort() {
+        for (int index = 0; index < size - 1; index++) {
+            indexMin = index;
+            for (int i = index + 1; i < size; i++) {
+                if (arr[i].doubleValue() < arr[indexMin].doubleValue()) {
+                    indexMin = i;
+                }
+            }
+
+            temp = arr[indexMin];
+            arr[indexMin] = arr[index];
+            arr[index] = temp;
+        }
+
+        return arr;
+    }
 }
