3b1c5da911a17b778e50d9e2c96d0238f22afd9f
OliaStfn <kardash44@mail.ru>
2017-05-19 19:34:55 +0300

    finish 27.03

--- src/main/java/com/stef/MagazineProject/mysql/MysqlProductDao.java
+++ src/main/java/com/stef/MagazineProject/mysql/MysqlProductDao.java
@@ -6,6 +6,7 @@ import com.stef.MagazineProject.domain.Product;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MysqlProductDao extends AbstractDao<Product, Integer> {
 
@@ -13,7 +14,7 @@ public class MysqlProductDao extends AbstractDao<Product, Integer> {
         super(connection);
     }
 
-    private class ProductForDB extends Product{
+    private class ProductForDB extends Product {
         public void setId(int id) {
             super.setId(id);
         }
@@ -60,12 +61,12 @@ public class MysqlProductDao extends AbstractDao<Product, Integer> {
     @Override
     public void statementUpdate(PreparedStatement statement, Product obj) throws DaoException {
         try {
-            statement.setString(1,obj.getName());
-            statement.setDouble(2,obj.getPrice());
-            statement.setString(3,obj.getVendor());
+            statement.setString(1, obj.getName());
+            statement.setDouble(2, obj.getPrice());
+            statement.setString(3, obj.getVendor());
             statement.setDate(4, Date.valueOf("2017-04-28"));
             statement.setDate(5, Date.valueOf("2017-04-30"));
-            statement.setInt(6,obj.getId());
+            statement.setInt(6, obj.getId());
         } catch (SQLException e) {
             throw new DaoException(e);
         }
@@ -74,9 +75,9 @@ public class MysqlProductDao extends AbstractDao<Product, Integer> {
     @Override
     public void statementInsert(PreparedStatement statement, Product obj) throws DaoException {
         try {
-            statement.setString(1,obj.getName());
-            statement.setDouble(2,obj.getPrice());
-            statement.setString(3,obj.getVendor());
+            statement.setString(1, obj.getName());
+            statement.setDouble(2, obj.getPrice());
+            statement.setString(3, obj.getVendor());
             statement.setDate(4, Date.valueOf("2017-04-28"));
             statement.setDate(5, Date.valueOf("2017-04-30"));
         } catch (SQLException e) {
@@ -87,7 +88,28 @@ public class MysqlProductDao extends AbstractDao<Product, Integer> {
     @Override
     public Product create() throws DaoException {
         Product tempProduct = new Product();
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter name of product: ");
+        String name = in.nextLine();
+        System.out.println("Enter price of product: ");
+        Double price = in.nextDouble();
+        System.out.println("Enter vendor of product: ");
+        String vendor = in.nextLine();
+        System.out.println("Enter production year of product: ");
+        int year = in.nextInt();
+        System.out.println("Enter production month of product: ");
+        int month = in.nextInt();
+        System.out.println("Enter production day of product: ");
+        int day = in.nextInt();
+        String prodDate = year + "-" + month + "-" + day;
+        System.out.println("Enter expiration date of product: ");
+        System.out.println("Enter expiration year of product: ");
+        int expyear = in.nextInt();
+        System.out.println("Enter expiration month of product: ");
+        int expmonth = in.nextInt();
+        System.out.println("Enter expiration day of product: ");
+        int expday = in.nextInt();
+        String expdate = expyear + "-" + expmonth + "-" + expday;
         return createInDB(tempProduct);
     }
 }
