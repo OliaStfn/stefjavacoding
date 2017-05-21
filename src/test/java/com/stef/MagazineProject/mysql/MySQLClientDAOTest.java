aa229b15a42279a4538ee506f8b20855b13ebc67
OliaStfn <kardash44@mail.ru>
2017-05-21 17:54:44 +0300

    finish 28.03

--- src/test/java/com/stef/MagazineProject/mysql/MySQLClientDAOTest.java
+++ src/test/java/com/stef/MagazineProject/mysql/MySQLClientDAOTest.java
@@ -1,6 +1,12 @@
 package com.stef.MagazineProject.mysql;
 
+import com.stef.MagazineProject.dao.GenericDao;
+import com.stef.MagazineProject.domain.Client;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.*;
 
@@ -10,27 +16,44 @@ import static org.junit.Assert.*;
 public class MySQLClientDAOTest {
     @Test
     public void createInDB() throws Exception {
-
+        Client pclient = new Client("Olga", "Stefanyshyn", 1999, 03, 22,
+                "0932202176", "Vovchynetska 198b/144");
+        MySQLDaoFactory factory = new MySQLDaoFactory();
+        GenericDao dao = factory.getDao(factory.getConnection(), Client.class);
+        Client fieldclient = (Client) dao.createInDB(pclient);
+        System.out.println(fieldclient.toString());
     }
 
     @Test
     public void read() throws Exception {
-
+        MySQLDaoFactory factory = new MySQLDaoFactory();
+        GenericDao dao = factory.getDao(factory.getConnection(), Client.class);
+        Client findclient = (Client) dao.read(1);
+        assertNotNull(findclient);
+        System.out.println(findclient.toString());
     }
 
     @Test
     public void update() throws Exception {
-
+        MySQLDaoFactory factory = new MySQLDaoFactory();
+        GenericDao dao = factory.getDao(factory.getConnection(), Client.class);
+        Client client = (Client) dao.read(1);
+        client.setName("Olya");
+        dao.update(client);
     }
 
     @Test
     public void delete() throws Exception {
-
+        MySQLDaoFactory factory = new MySQLDaoFactory();
+        GenericDao dao = factory.getDao(factory.getConnection(), Client.class);
+        dao.delete(2);
     }
 
     @Test
     public void readAll() throws Exception {
-
+        MySQLDaoFactory factory = new MySQLDaoFactory();
+        GenericDao dao = factory.getDao(factory.getConnection(), Client.class);
+        ArrayList<Client> clients = dao.readAll();
+        assertNotNull(clients);
     }
-
 }
