880cb04281cde7062206e3fcba131080020cca0c
NazarMh <nazar.myhailiv@gmail.com>
2017-06-16 23:37:27 +0300

    Meeting 36

---
+++ src/test/java/com/stef/Meetings/lesson_33/LinkedListQueueTest.java
@@ -0,0 +1,22 @@
+package com.stef.Meetings.lesson_33;
+
+import com.stef.MagazineProject.domain.Client;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkedListQueueTest {
+    @Test
+    public void add() throws Exception {
+        LinkedListQueue<Client> clientQueue=new LinkedListQueue<Client>();
+        clientQueue.add(new Client());
+        clientQueue.add(new Client());
+        clientQueue.add(new Client());
+
+        while (!clientQueue.isEmpty()){
+            System.out.println(clientQueue.remove().toString());
+            System.out.println("Queue of clients now: "+clientQueue.size()+"\n");
+        }
+    }
+
+}
