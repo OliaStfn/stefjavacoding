c6b388bef8662e59283ab3ace20beb27271189d0
OliaStfn <kardash44@mail.ru>
2017-06-23 13:38:31 +0300

    finish 37.02

--- src/main/java/com/stef/Meetings/lesson_36_1/LinkedListDeque.java
+++ src/main/java/com/stef/Meetings/lesson_36_1/LinkedListDeque.java
@@ -8,13 +8,82 @@ public class LinkedListDeque<T> {
     private class Node {
         T item;
         Node next;
+        Node previous;
+    }
 
-        public Node(T item) {
-            this.item = item;
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addFirst(T item) {
+        if (!isNull(item)) {
+            Node newFirst = new Node();
+            newFirst.item = item;
+
+            if (first != null) {
+                newFirst.next = first;
+                first.previous = newFirst;
+            }
+            first = newFirst;
+            if (last == null) last = first;
+
+            size++;
         }
     }
 
-    public LinkedListDeque() {
+    public T removeFirst() {
+        if (!isEmpty()) {
+            Node oldFirst = first;
+            first = first.next;
+
+            if (first == null){
+                last = null;
+            }
+            else
+                first.previous = null;
+            size--;
+
+            return oldFirst.item;
+        }
+        return null;
     }
 
+    public void addLast(T item) {
+        if(!isNull(item)){
+            Node newLast = new Node();
+            newLast.item = item;
+
+            if (last != null) {
+                newLast.previous = last;
+                last.next = newLast;
+            }
+            last = newLast;
+            if (first == null) first = last;
+
+            size++;
+        }
+    }
+
+    public T removeLast() {
+        if (!isEmpty()) {
+            Node oldLast = last;
+            last = oldLast.previous;
+
+            if (last == null)
+                first = null;
+            else
+                last.next = null;
+            size--;
+            return oldLast.item;
+        }
+        return null;
+    }
+
+    private boolean isNull(T item) {
+        return (item == null);
+    }
 }
